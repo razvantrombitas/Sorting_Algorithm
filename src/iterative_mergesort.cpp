8fdc44174e8f926dd2f57f28c41b6059aff751a8
razvantrombitas <razvan.trombitas@stud.etti.upb.ro>
2022-06-21 09:07:39 +0000

    src folder added

---
+++ src/iterative_mergesort.cpp
@@ -0,0 +1,113 @@
+// Iterative Merge Sort 
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+#define N 500000
+uint32_t arr[N];
+
+int min(int x, int y) { return (x<y)? x :y; }
+
+/* Function to merge the two haves arr[l..m] and arr[m+1..r] of array arr[] */
+template <class T> void merge(T arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
+template <class T> void mergeSort(T arr[], int n)
+{
+    int curr_size; // curr_size varies from 1 to n/2
+    int left_start; // starting index of left subarray to be merged
+
+    // Merge subarrays in bottom up manner.  First merge subarrays of
+    // size 1 to create sorted subarrays of size 2, then merge subarrays
+    // of size 2 to create sorted subarrays of size 4, and so on.
+
+    for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
+    {
+        // Pick starting point of different subarrays of current size
+        #pragma omp parallel for
+        for (left_start=0; left_start<n-1; left_start += 2*curr_size)
+        {
+            // Find ending point of left subarray. mid+1 is starting point of right
+            int mid = min(left_start + curr_size - 1, n-1);
+            int right_end = min(left_start + 2*curr_size - 1, n-1);
+
+            // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
+            merge(arr, left_start, mid, right_end);
+        }
+    }
+}
+
+template <class T> void printArray(T A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout <<" "<< A[i];
+    cout <<"\n";
+}
+
+int main() {
+
+    for (int i = 0; i < N; i++)
+        arr[i] = (uint32_t) rand() * rand() ;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort<uint32_t>(arr, N);
+    auto stop = std::chrono::high_resolution_clock::now();
+    cout << "Done in " << std::chrono::duration_cast<std::chrono::milliseconds>(stop-start).count() << " ms" << std::endl;
+
+//    cout << "Sorted array: \n";
+//    printArray<uint32_t>(arr, N);
+
+    return 0;
+}
---
+++ src/recursive_mergesort.cpp
@@ -0,0 +1,86 @@
+// Recursive Merge Sort
+#include <iostream>
+#include <chrono>
+using namespace std;
+#define N 500000
+uint32_t arr[N];
+
+// Merge two subarrays L and M into arr
+template <class T> void merge(T arr[], int p, int q, int r) {
+
+    // Create L ← A[p..q] and M ← A[q+1..r]
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= M[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+// Divide the array into two subarrays, sort them and merge them
+template <class T> void mergeSort(T arr[], int l, int r) {
+    if (l < r) {
+        
+        int m = l + (r - l) / 2;
+        mergeSort<T>(arr, l, m);
+        mergeSort<T>(arr, m + 1, r);
+
+        // Merge the sorted subarrays
+        if (arr[m] > arr[m + 1]) // optimization: the array is already sorted; by adding this if O(n) time complexity could be obtained in best case
+        	merge<T>(arr, l, m, r);
+    }
+}
+template <class T> void printArray(T arr[], int size) {
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+
+    for (int i = 0; i < N; i++)
+        arr[i] = (uint32_t) rand() ;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort<uint32_t>( arr, 0, N - 1);
+    auto stop = std::chrono::high_resolution_clock::now();
+    cout << "Done in " << std::chrono::duration_cast<std::chrono::milliseconds>(stop-start).count() << " ms" << std::endl;
+
+//    cout << "Sorted array: \n";
+//    printArray<uint32_t>(arr, N);
+    return 0;
+}
